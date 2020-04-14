b3859f273f9176c38e85e5ae0adf570e613b6aed
Hammadsoloman <hammadali95@yahoo.com>
2020-04-15 00:58:28 +0300

    finish lab12

--- server.js
+++ server.js
@@ -1,32 +1,41 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
-
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 3030;
 const app = express();
 const superagent = require('superagent');
-
+const pg  = require('pg');
 app.use(express.static('./public'));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+const client = new pg.Client(process.env.DATABASE_URL);
 app.set('view engine', 'ejs');
+var bookgallery=[];
 
-app.get('/', (req, res) => {
-    res.render('pages/index');
+app.get('/',(req,res)=>{
+res.redirect('/index')
 })
+app.get('/index', (req, res) => {
+    let SQL = 'SELECT * FROM books;';
+    return client.query(SQL)
+    .then(results =>{
+        res.render('pages/index',{books:results.rows});
+    })
+})
+
 app.get('/search', (req, res) => {
-    res.render('pages/searches/new.ejs')
+    res.render('pages/searches/new')
 })
+
 
 app.post('/searches', (req, res) => {
+
     var url;
-    if (req.body.myBook === 'title') {
+    if (req.body.searchtype === 'title') {
         url = `https://www.googleapis.com/books/v1/volumes?q=${req.body.bookname}&intitle:${req.body.bookname}`;
     }
-    else if (req.body.myBook === 'author') {
-        url = `https://www.googleapis.com/books/v1/volumes?q=${req.body.bookname}&inauthor:${req.body.bookname}`;
+    else if (req.body.searchtype === 'author') {
+        url = `https://www.googleapis.com/books/v1/volumes?q=${req.body.bookname}+inauthor:${req.body.bookname}`;
     }
 
     superagent.get(url)
@@ -34,26 +43,53 @@ app.post('/searches', (req, res) => {
             let arr = data.body.items;
             let books = arr.map(book => {
                 let bookitem = new Book(book);
-                console.log(book.length);
+                bookgallery.push(bookitem);
                 return bookitem;
             });
-            res.render('pages/searches/show', { books: books })
+            res.render('pages/searches/detail', { books: books })
         })
         .catch(error => {
             res.render('pages/error');
         });
 });
+
+app.get('/books/:book_id', getbookDetails);
+function getbookDetails(req,res) {
+    let SQL = 'SELECT * FROM books WHERE id=$1;';
+    let values = [req.params.book_id];
+    return client.query(SQL,values)
+    .then (result =>{
+        
+        res.render('/books/detail',{details:result.rows[0]});
+    })
+}
+
+app.post('/books', insertDetails);
+
+function insertDetails(req,res) {
+    let {title,author,isbn,image_url,description} = req.body;
+    let SQL = 'INSERT INTO books (title,author,isbn,image_url,description) VALUES ($1,$2,$3,$4,$5);';
+    let safeValues = [title,author,isbn,image_url,description];
+    return client.query(SQL,safeValues)
+    .then (()=>{
+        res.redirect('/index');
+    })
+        
+ }
 function Book(book) {
     this.title = book.volumeInfo.title;
     this.smallThumbnail = book.volumeInfo.imageLinks.smallThumbnail;
     this.authors = book.volumeInfo.authors;
     this.description = book.volumeInfo.description;
-
+    this.isbn=book.volumeInfo.industryIdentifiers[0].type;
 }
 app.get('*', (req, res) => {
-    res.status(404).send('You have a error in writing the route');
+    res.status(404).send('This route does not exist!!');
 })
 
-app.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`)
+client.connect()
+.then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Listening on PORT ${PORT}`);
+    })
 })
